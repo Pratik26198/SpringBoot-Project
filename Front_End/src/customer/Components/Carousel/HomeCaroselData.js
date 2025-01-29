9c0882849952797803c42794623d34e4e1f997cf
Pratik Shahane <pratik26198@gmail.com>
2025-01-30 01:17:27 +0530

    Add files via upload

---
+++ Front_End/src/customer/Components/Carousel/HomeCaroselData.js
@@ -0,0 +1,22 @@
+export const homeCarouselData = [
+  {
+    type: "video",
+    src: "https://pub-fcbdfa5d08884a7fb45a0457f296badb.r2.dev/video/b/1725598946516.mp4",
+    path: "/women/rings/engagement",
+  },
+  {
+    type: "image",
+    src: "https://cdn.bradojewellery.com/b/1920x/1725540401775.jpeg",
+    path: "/women/rings/engagement",
+  },
+  {
+    type: "image",
+    src: "https://cdn.bradojewellery.com/b/1920x/1725537269157.jpeg",
+    path: "/women/rings/engagement",
+  },
+  {
+    type: "image",
+    src: "https://cdn.bradojewellery.com/b/1920x/1725537345613.jpeg",
+    path: "/women/rings/engagement",
+  },
+];
